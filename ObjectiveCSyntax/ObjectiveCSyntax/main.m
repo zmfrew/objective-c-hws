3ccbf742600cfa684e282d6274e629ab40647da5
Zach Frew <zachary.frew@ascension-external.org>
2020-04-15 08:42:31 -0500

    Review basic syntax for vars, loops, and switch statements

--- ObjectiveCSyntax/ObjectiveCSyntax/main.m
+++ ObjectiveCSyntax/ObjectiveCSyntax/main.m
@@ -1,17 +1,37 @@
-//
-//  main.m
-//  ObjectiveCSyntax
-//
-//  Created by Zachary Frew on 4/15/20.
-//  Copyright © 2020 Zach Frew. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int i = 10;
+        if (i == 10) {
+            NSLog(@"Hello, World!");
+        }
+        
+        switch (i) {
+            case 1 ... 9:
+                NSLog(@"It's between 1 & 30!");
+                break;
+                
+            case 10:
+            {
+                int foo = 1;
+                NSLog(@"I need curly braces here in order to assign a new variable inside a switch case.");
+            }
+                
+            case 11:
+                NSLog(@"Or we can do it this way with another statement preceding the new variable.");
+                int bar = 2;
+                
+            default:
+                NSLog(@"It's something else");
+        }
+        
+        NSString *str = @"Reject common sense to make the impossible possible!";
+        
+        NSArray *names = @[@"Laura", @"Janet", @"Kim"];
+        for (NSString *name in names) {
+            NSLog(@"Hello, %@", name);
+        }
     }
     return 0;
 }
