eaeeffc888b7b84b325182aca18d955c7db5578b
Zach Frew <zachary.frew@ascension-external.org>
2020-04-15 08:20:25 -0500

    Basic syntax project setup

---
+++ ObjectiveCSyntax/ObjectiveCSyntax/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveCSyntax
+//
+//  Created by Zachary Frew on 4/15/20.
+//  Copyright © 2020 Zach Frew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
