d718beb6e91443ac3695f4fb028329d6ebabc515
Zach Frew <zachary.frew@ascension-external.org>
2020-04-25 08:13:52 -0500

    Sample syntax for blocks

--- ObjectiveCSyntax/ObjectiveCSyntax/main.m
+++ ObjectiveCSyntax/ObjectiveCSyntax/main.m
@@ -2,36 +2,65 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int i = 10;
-        if (i == 10) {
-            NSLog(@"Hello, World!");
-        }
+        // Typealias
+        typedef NSString* (^StringBlock)(NSString*);
         
-        switch (i) {
-            case 1 ... 9:
-                NSLog(@"It's between 1 & 30!");
-                break;
-                
-            case 10:
-            {
-                int foo = 1;
-                NSLog(@"I need curly braces here in order to assign a new variable inside a switch case.");
-            }
-                
-            case 11:
-                NSLog(@"Or we can do it this way with another statement preceding the new variable.");
-                int bar = 2;
-                
-            default:
-                NSLog(@"It's something else");
-        }
+        // Blocks
+        StringBlock printUniversalGreeting = ^(NSString *name) {
+            return [NSString stringWithFormat:@"Welcome to Skull Island, %@.", name];
+        };
         
-        NSString *str = @"Reject common sense to make the impossible possible!";
+        NSLog(@"%@", printUniversalGreeting(@"Zach"));
         
-        NSArray *names = @[@"Laura", @"Janet", @"Kim"];
-        for (NSString *name in names) {
-            NSLog(@"Hello, %@", name);
-        }
+        // Capturing Values
+        NSString *name = @"Finn";
+        
+        NSString* (^greetings)(void) = ^{
+            return [NSString stringWithFormat:@"%@, go!", name];
+        };
+        NSLog(@"%@", greetings());
+        
+        // Modifying Values in a Block
+        NSInteger __block number = 0;
+        NSString* (^printMeaningOfLife)(void) = ^{
+            number = 42;
+            return [NSString stringWithFormat:@"How many roads must a man walk down? %ld.", number];
+        };
+        NSLog(@"%@", printMeaningOfLife());
     }
     return 0;
 }
+
+/*
+ int i = 10;
+ if (i == 10) {
+     NSLog(@"Hello, World!");
+ }
+ 
+ switch (i) {
+     case 1 ... 9:
+         NSLog(@"It's between 1 & 30!");
+         break;
+         
+     case 10:
+     {
+         int foo = 1;
+         NSLog(@"I need curly braces here in order to assign a new variable inside a switch case.");
+     }
+         
+     case 11:
+         NSLog(@"Or we can do it this way with another statement preceding the new variable.");
+         int bar = 2;
+         
+     default:
+         NSLog(@"It's something else");
+ }
+ 
+ NSString *str = @"Reject common sense to make the impossible possible!";
+ 
+ NSArray *names = @[@"Laura", @"Janet", @"Kim"];
+ for (NSString *name in names) {
+     NSLog(@"Hello, %@", name);
+ }
+ 
+ */
